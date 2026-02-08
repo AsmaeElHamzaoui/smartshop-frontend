f164f63d2ef30ffe20bc31234eb4dc8e57b73583
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-02-09 00:18:24 +0100

    l'ajout de client , order anisi que product reducer dans index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import clientReducer from './slices/clientSlice';
-
+import userReducer from './slices/userSlice';
+import productReducer from './slices/productSlice';
+import orderReducer from './slices/orderSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer,
     // Ajouter d'autres reducers ici
-    // products: productReducer,
-    // orders: orderReducer,
+     users: userReducer,
+     products: productReducer,
+     orders: orderReducer,
      clients: clientReducer,
   },
   middleware: (getDefaultMiddleware) =>
